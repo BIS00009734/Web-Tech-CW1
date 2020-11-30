60a113dc5ef1798cc687f9ad2f8b19206e9efcae
00009734 <72858174+BIS00009734@users.noreply.github.com>
2020-11-30 22:47:17 +0500

    films cartoon serials done

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ var showcaseitems = {
     Img3: ['solar.html', 'img/solar.jpg', 'Solar', 'Solar Opposites 2020']
 }
 
+
 // Changing showcase
 function showcaseChange (sideLi) { 
 
+    // swaps showcase items
     if (sideLi == 0) {
         showcaseitems.Img1 = [...showcaseitems.Img2, showcaseitems.Img2 = showcaseitems.Img1]
     } else {
         showcaseitems.Img3 = [...showcaseitems.Img2, showcaseitems.Img2 = showcaseitems.Img3]
     }
 
-
-
-
+    //reassigning showcase items
     document.getElementById('showcaseImg1').src = showcaseitems.Img1[1]
     document.getElementById('showcaseImg2').src = showcaseitems.Img2[1]
     document.getElementById('showcaseImg3').src = showcaseitems.Img3[1]
